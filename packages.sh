e8b256583d5cb0597f8e311c1e02af593064e9f8
Unbê Produções <backup.arquivosjm@gmail.com>
2021-11-13 17:46:07 -0300

    whitelist folder added

--- packages.sh
+++ packages.sh
@@ -85,24 +85,3 @@ iptables -A INPUT -p icmp -m limit --limit 2/second -j ACCEPT
 
 sudo /etc/init.d/iptables-persistent save
 sleep 5
-
-echo "██╗    ██╗██╗  ██╗██╗████████╗███████╗██╗     ██╗███████╗████████╗"
-echo "██║    ██║██║  ██║██║╚══██╔══╝██╔════╝██║     ██║██╔════╝╚══██╔══╝"
-echo "██║ █╗ ██║███████║██║   ██║   █████╗  ██║     ██║███████╗   ██║   "
-echo "██║███╗██║██╔══██║██║   ██║   ██╔══╝  ██║     ██║╚════██║   ██║   "
-echo "╚███╔███╔╝██║  ██║██║   ██║   ███████╗███████╗██║███████║   ██║   "
-echo " ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝   ╚═╝   ╚══════╝╚══════╝╚═╝╚══════╝   ╚═╝   "
-
-# Colocar na lista branca os ips da cloudflare
-
-# Coloque abaixo o endereço IP da sua máquina!
-SERVER_IP=168.0.0.1
-
-for i in $(curl "https://www.cloudflare.com/ips-v4"); do sudo iptables --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="'$i'" port port=80 protocol=tcp accept'; done
-for i in $(curl "https://www.cloudflare.com/ips-v4"); do sudo iptables --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="'$i'" port port=443 protocol=tcp accept'; done
-
-for i in $(curl "https://www.cloudflare.com/ips-v6"); do sudo iptables --permanent --zone=public --add-rich-rule 'rule family="ipv6" source address="'$i'" port port=80 protocol=tcp accept'; done
-for i in $(curl "https://www.cloudflare.com/ips-v6"); do sudo iptables --permanent --zone=public --add-rich-rule 'rule family="ipv6" source address="'$i'" port port=443 protocol=tcp accept'; done
-
-iptables --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="'${SERVER_IP}'" port port=22 protocol=tcp accept'
-iptables --permanent --change-zone=eth0 --zone=public
